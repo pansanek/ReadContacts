52376e3702a31f617164832e0f91117ad8f64d27
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-04 13:20:06 +0300

    Проверка разрешений

--- app/src/main/java/ru/potemkin/readcontacts/MainActivity.kt
+++ app/src/main/java/ru/potemkin/readcontacts/MainActivity.kt
@@ -1,17 +1,27 @@
 package ru.potemkin.readcontacts
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.provider.ContactsContract.Contacts
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestContacts()
+        val permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+        if (permissionGranted){
+            requestContacts()
+        } else{
+            Log.d("MainActivity","Permission denied")
+        }
     }
 
     private fun requestContacts() {
